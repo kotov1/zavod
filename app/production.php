9aae46baad4efd21740b6645ab0fab760a8bf272
Petr Petukhov <kotov0991@gmail.com>
2018-07-17 23:54:47 +0300

    p-production full

---
+++ app/production.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<meta property="og:image" content="img/opengraph.jpg">
+
+	<link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+	<link rel="manifest" href="img/favicon/site.webmanifest">
+	<link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<!-- Chrome, Firefox OS and Opera -->
+	<meta name="theme-color" content="#ffffff">
+	<!-- Windows Phone -->
+	<meta name="msapplication-navbutton-color" content="#000">
+	<!-- iOS Safari -->
+	<meta name="apple-mobile-web-app-status-bar-style" content="#000">
+
+	<title>Продукция завода</title>
+
+	<!-- bootstrap grid css -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap-grid.min.css">
+
+	<link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body>
+
+	<div class="page main-page">
+
+
+		<!-- header -->
+		<?php include("blocks/header.php");?>
+		<!-- header -->
+
+		<div class="overlay"></div>
+
+
+
+
+		<!-- content -->
+		<main class="content">
+
+
+				<!-- page-banner -->
+			<section class="page-banner text-center" style="background-image: url('img/test2.jpg')">
+				<div class="container">
+					<h1 class="page-title">Продукция завода</h1>
+					<div itemscope="" itemtype="http://schema.org/BreadcrumbList" class="breadcrumbs">
+						<span itemscope="" itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+							<a rel="nofollow" itemprop="item" title="Главная" href="#">
+								<span itemprop="name">Главная</span>
+								<meta itemprop="position" content="1">
+							</a>
+							</span>
+							<span itemscope="" itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+							<a rel="nofollow" itemprop="item" title="Продукция завода" href="#">
+								<span itemprop="name">Продукция завода</span>
+								<meta itemprop="position" content="2">
+							</a>
+							</span>
+					</div>
+				</div>
+			</section>
+			<!-- page-banner end -->
+
+
+
+			<!-- categories-section -->
+			<section class="categories">
+				<div class="container">
+					<div class="categories__items">
+						<div class="row">
+
+							<div class="col-12 col-md-6">
+								<div class="page-categories__block">
+									<div class="categories__item align-items-lg-center">
+										<div class="categories__name">Рукава высокого давления (РВД) в сборе</div>
+										<div class="categories__img">
+											<img src="img/categories/category-img1.png" class="img-responsive" alt="Логотип" 
+														srcset="img/categories/category-img1.png 1x, img/categories/category-img1@2x.png 2x">
+										</div>
+										<div class="categories__link">
+											<a href="#">К разделу</a>
+										</div>
+									</div>
+									<ul class="categories__list">
+										<li>РВД в сборе с фитингами из нерж. стали</li>
+										<li>РВД в сборе с сертификатом РМРС</li>
+										<li>Рукавные соединения</li>
+										<li>Судовые шланги</li>
+										<li>РВД с нестандартными окончаниями</li>
+									</ul>
+								</div>
+							</div>
+							
+
+							<div class="col-12 col-md-6">
+								<div class="page-categories__block">
+									<div class="categories__item align-items-lg-center">
+										<div class="categories__name">Металлорукава высокого давления (МРВД)</div>
+										<div class="categories__img">
+											<img src="img/categories/category-img3.png" class="img-responsive" alt="Логотип" 
+														srcset="img/categories/category-img3.png 1x, img/categories/category-img3@2x.png 2x">
+										</div>
+										<div class="categories__link">
+											<a href="#">К разделу</a>
+										</div>
+									</div>
+									<ul class="categories__list">
+										<li>Под приварку</li>
+										<li>С фланцами</li>
+										<li>С резьбовыми соединениями</li>
+										<li>С отводами</li>
+										<li>С БРС Camlock</li>
+										<li>МРВД стандартной гибкости (100)</li>
+										<li>МРВД усиленный (200)</li>
+										<li>МРВД повышенной гибкости (300) </li>
+									</ul>
+								</div>
+							</div>
+
+							
+							<div class="col-12 col-md-6">
+								<div class="page-categories__block">
+									<div class="categories__item align-items-lg-center">
+										<div class="categories__name">Сильфонные компенсаторы</div>
+										<div class="categories__img bottom-0">
+											<img src="img/categories/category-img5.png" class="img-responsive" alt="Логотип" 
+														srcset="img/categories/category-img5.png 1x, img/categories/category-img5@2x.png 2x">
+										</div>
+										<div class="categories__link">
+											<a href="#">К разделу</a>
+										</div>
+									</div>
+									<ul class="categories__list">
+										<li>Осевые (КСО)</li>
+										<li>Стартовые (ССК)</li>
+										<li>Сдвиговые (КСС)</li>
+										<li>Угловые (КСП)</li>
+										<li>Карданные (КСПК)</li>
+										<li>Разгруженные (КСР)</li>
+										<li>Компенсационные устройства</li>
+									</ul>
+								</div>
+							</div>
+							
+
+							<div class="col-12 col-md-6">
+								<div class="page-categories__block">
+									<div class="categories__item align-items-lg-center">
+										<div class="categories__name">Фитинги и муфты для рукавов высокого давления (РВД)</div>
+										<div class="categories__img bottom-0">
+											<img src="img/categories/category-img2.png" class="img-responsive" alt="Логотип" 
+														srcset="img/categories/category-img2.png 1x, img/categories/category-img2@2x.png 2x">
+										</div>
+										<div class="categories__link">
+											<a href="#">К разделу</a>
+										</div>
+									</div>
+									<ul class="categories__list">
+										<li>Муфты из нерж. стали для РВД </li>
+										<li>Фитинги из нерж. стали для РВД </li>
+										<li>Нестандартные фитинги</li>
+									</ul>
+								</div>
+							</div>
+
+
+							<div class="col-12 col-md-6">
+								<div class="page-categories__block">
+									<div class="categories__item align-items-lg-center">
+										<div class="categories__name">Трубные соединения</div>
+										<div class="categories__img">
+											<img src="img/categories/category-img4.png" class="img-responsive" alt="Логотип" 
+														srcset="img/categories/category-img4.png 1x, img/categories/category-img4@2x.png 2x">
+										</div>
+										<div class="categories__link">
+											<a href="#">К разделу</a>
+										</div>
+									</div>
+									<ul class="categories__list">
+										<li>Тип 1</li>
+										<li>Тип 2</li>
+										<li>Тип 3</li>
+									</ul>
+								</div>
+							</div>
+
+
+							<div class="col-12 col-md-6">
+								<div class="page-categories__block">
+									<div class="categories__item align-items-lg-center">
+										<div class="categories__name">Скачать каталоги продукции</div>
+										<div class="categories__img">
+											<img src="img/categories/category-img6.png" class="img-responsive" alt="Логотип" 
+														srcset="img/categories/category-img6.png 1x, img/categories/category-img6@2x.png 2x">
+										</div>
+										<div class="categories__link">
+											<a href="#">К разделу</a>
+										</div>
+									</div>
+									<ul class="categories__list">
+										<li>Каталог 1</li>
+										<li>Каталог 2</li>
+										<li>Каталог 3</li>
+									</ul>
+								</div>
+							</div>
+
+						</div>
+						<!-- /.row -->
+					</div>
+					<!-- /.categories__items -->
+				</div>
+				<!-- /.container -->
+			</section>
+			<!-- /.categories-section -->
+
+
+
+			<!-- form-section -->
+			<?php include("blocks/form-section.php");?>
+			<!-- form-section end -->
+
+
+
+			<div class="content-block">
+				<div class="container">
+					<p class="content-text">Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами. Какой-то большой текст для SEO про компанию с ключевыми словами.</p>
+				</div>
+			</div>
+
+
+		
+		</main>
+		<!-- content end -->
+	
+
+
+		<!-- footer -->
+		<?php include("blocks/footer.php");?>
+		<!-- /.footer -->
+
+
+
+
+	</div>
+	<!-- /.page-main -->
+
+
+
+
+
+	<!-- popup callback begin -->
+	<?php include("blocks/modal-callback.php");?>
+	<!-- popup callback end -->
+
+	<!-- popup thanks begin -->
+	<?php include("blocks/modal-thanks.php");?>
+	<!-- popup thanks end -->
+
+
+		
+
+
+
+	
+
+
+	<!-- Jquery с облака и замена в случае отсутствия доступа к облаку  -->
+	<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script>window.jQuery || document.write('<script src="libs/jquery/dist/jquery.min.js"><\/script>')</script>
+
+	<script src="js/scripts.js"></script>
+
+</body>
+</html>
